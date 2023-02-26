6df147f1c0c6be26e1882241d6e180539e992293
Hirchenko <hirchenko17@gmail.com>
2023-02-26 23:03:08 +0200

    Виконано калькуляцію двох чисел з різними операціями, квадратний корінь з num1, підведення num1 до степені num2. Автооновлення результату в калькуляторі.

---
+++ resources/calc.js
@@ -0,0 +1,41 @@
+ function calulate(){
+	const num1=+document.querySelector('[name="num1"]').value;
+	const num2=+document.querySelector('[name="num2"]').value;
+	const op=document.querySelector('[name="op"]').value;
+	
+	let result;
+	
+	if (op === "plus") {
+		result = num1 + num2;
+	} else if (op === "minus") {
+		result = num1 - num2;
+		} else if (op === "multiple") {
+		result = num1 * num2;
+		} else if (op === "divide") {
+		result = num1 / num2;
+		} else if (op === "sqrt") {
+		result =  Math.sqrt (num1);
+		} else if (op === "exponent") {
+		result = Math.pow(num1, num2);
+		} else  {
+		console.error ('Wrong op!');
+		alert ('Wrong operation is selected!');
+		}
+	
+	
+	
+	document.querySelector('.calc-result').value=result;
+}
+ 
+ document.querySelector('.calc-calculate').addEventListener('click', function () {
+	 calulate ()
+});
+document.querySelector('[name="op"]').addEventListener('change', function () {
+	 calulate ()
+});
+document.querySelector('[name="num1"]').addEventListener('input', function () {
+	 calulate ()
+});
+document.querySelector('[name="num2"]').addEventListener('input', function () {
+	 calulate ()
+});
