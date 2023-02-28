af011748e507e981f581a420408aa7376f431426
Viacheslav Khyzhniak <slvpost86@gmail.com>
2023-02-28 16:23:11 +0000

    Added different operators

--- resources/clc.js
+++ resources/clc.js
@@ -1,6 +1,20 @@
 document.querySelector('.clc-clct').addEventListener('click', function(){
     const num1 = document.querySelector('[name="num1"]').value;
     const num2 = document.querySelector('[name="num2"]').value;
-    const sum = parseInt(num1) + parseInt(num2);
-    document.querySelector('.clc-rslt').value = sum;
+    const opr = document.querySelector('[name="opr"]').value;
+
+    let rslt;
+    if(opr === 'plus'){
+        rslt = parseInt(num1) + parseInt(num2);
+    } else if(opr === 'minus'){
+        rslt = parseInt(num1) - parseInt(num2);
+    } else if(opr === 'multiply'){
+        rslt = parseInt(num1) * parseInt(num2);
+    } else if(opr === 'divide'){
+        rslt = parseInt(num1) / parseInt(num2);
+    } else {
+        console.error('Smthng wrng');
+        alert('Wrong operation is selected');
+    }
+    document.querySelector('.clc-rslt').value = rslt;
 });
